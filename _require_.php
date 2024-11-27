cb819e248bc9107e61be4f45b2a606c7ee3e1189
Jon Surrell <sirreal@users.noreply.github.com>
2024-11-27 13:25:37 +0100

    improvements

--- _require_.php
+++ _require_.php
@@ -7,10 +7,11 @@
 namespace sirreal;
 
 function dt( ...$data ): void {
+	d( ...$data );
 	$trace = debug_backtrace( 0, 2 );
 	array_shift( $trace );
 	foreach ( $trace as $t ) {
-		$file     = substr( $t['file'], strlen( \ABSPATH ) );
+		$file     = defined( 'ABSPATH' ) ? substr( $t['file'], strlen( \ABSPATH ) ) : $t['file'];
 		$line     = $t['line'];
 		$function = $t['function'];
 		$class    = $t['class'] ?? '';
@@ -30,10 +31,16 @@ function dt( ...$data ): void {
 		$call .= $function;
 		$call .= $args === '' ? '()' : "( {$args} )";
 
-		State::print( "{$call} {$file}:{$line}\n" );
+		$j = $i;
+		State::print( "\e[32;2m" );
+		while ( $j-- ) {
+			State::print( '  ' );
+		}
+		State::print( "↳ {$call} {$file}:{$line}\e[0m\n" );
+		++$i;
 	}
-	d( ...$data );
 }
+
 function d( ...$data ): void {
 	ob_start();
 	foreach ( $data as $d ) {
@@ -43,31 +50,48 @@ function d( ...$data ): void {
 	State::print( ob_get_clean() );
 }
 
+/**
+ * Gated debug function
+ *
+ * Use \sirreal\gate(true|false) to enable or disable debug output
+ */
+function dg( ...$data ): void {
+	if ( State::$log_enabled ) {
+		d( ...$data );
+	}
+}
+
+function gate( bool $gate ): void {
+	State::$log_enabled = $gate;
+}
+
 function dx( ...$args ): void {}
 
-\add_action( 'init', array( State::class, 'init' ) );
-
-\add_action( 'shutdown', array( State::class, 'shutdown' ) );
+if ( function_exists( 'add_action' ) ) {
+	\add_action( 'init', array( State::class, 'init' ) );
+	\add_action( 'shutdown', array( State::class, 'shutdown' ) );
+}
 
 abstract class State {
 	private static $printed_header = false;
+	private static $log_location   = '/tmp/sireal-debug-log.txt';
+	private static $uri            = null;
+	private static $time           = null;
+	private static $method         = null;
 
-	private static $uri    = null;
-	private static $time   = null;
-	private static $method = null;
-
-	const LOG_LOCATION = \WP_CONTENT_DIR . '/log.txt';
+	public static $log_enabled = false;
 
 	private static function write_to_log( string $s ) {
-		file_put_contents( self::LOG_LOCATION, $s, FILE_APPEND );
+		file_put_contents( self::$log_location, $s, FILE_APPEND );
 	}
 
 	public static function print( $val ) {
 		if ( ! self::$printed_header ) {
 			self::$printed_header = true;
-			$header               = "=======================================\n" .
+
+			$header = "\e[32;2m=======================================\n" .
 			'START: ' . self::$time . ' ' . self::$method . ' ' . self::$uri . "\n" .
-			"=======================================\n";
+			"=======================================\e[0m\n";
 
 			self::write_to_log( $header );
 		}
@@ -78,6 +102,10 @@ abstract class State {
 		self::$uri    = $_SERVER['REQUEST_URI'];
 		self::$time   = $_SERVER['REQUEST_TIME'];
 		self::$method = $_SERVER['REQUEST_METHOD'];
+
+		if ( defined( 'WP_CONTENT_DIR' ) ) {
+			self::$log_location = \WP_CONTENT_DIR . '/uploads/log.txt';
+		}
 	}
 
 	public static function shutdown() {
@@ -85,9 +113,9 @@ abstract class State {
 			return;
 		}
 		self::write_to_log(
-			"=======================================\n" .
+			"\e[32;2m=======================================\n" .
 			'END: ' . self::$time . ' ' . self::$method . ' ' . self::$uri . "\n" .
-			"=======================================\n"
+			"=======================================\e[0m\n"
 		);
 	}
 }
